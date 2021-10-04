4dbb363049f7e965034c55b453e4a2ffbea17a1a
ktodoran <ktodoran@gmail.com>
2021-10-04 16:39:28 -0400

    Fixed rest of server files

--- server/server.js
+++ server/server.js
@@ -2,9 +2,10 @@ const express = require('express');
 const path = require('path');
 const { ApolloServer } = require('apollo-server-express');
 const { typeDefs, resolvers } = require('./schemas');
+const routes = require('./routes');
+const { authMiddleware } = require('./utils/auth');
 
 const db = require('./config/connection');
-const { authMiddleware } = require('./utils/auth');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -18,26 +19,27 @@ const server = new ApolloServer({
 async function startApolloServer(typeDefs, resolvers) {
   await server.start();
   await server.applyMiddleware({
-    app,
+    app
   });
 };
 
-startApolloServer(typeDefs, resolvers);
-
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
+
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
+
 db.once('open', () => {
   app.listen(PORT, () => {
     console.log(`🌍 Now listening on localhost:${PORT}`);
+    // log where we can go to test our GQL API
     console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
   });
 });
--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,32 +1,31 @@
 const jwt = require('jsonwebtoken');
-
 const secret = 'mysecretsshhhhh';
 const expiration = '2h';
 
 module.exports = {
-  signToken: function ({ username, email, _id }) {
-    const payload = { username, email, _id };
-
-    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
-  },
-  authMiddleware: function ({ req }) {
-    let token = req.body.token || req.query.token || req.headers.authorization;
-
+  authMiddleware: function (req, res, next) {
+    let token = req.query.token || req.headers.authorization;
     if (req.headers.authorization) {
       token = token.split(' ').pop().trim();
     }
 
     if (!token) {
-      return req;
+      return res.status(400).json({ message: 'What happened to your Token?!' });
     }
+
     try {
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
       req.user = data;
     } catch {
-      console.log('Invalid token');
-      return res.status(400).json({ message: 'invalid token!' });
+      console.log('Invalid Token!');
+      return res.status(400).json({ message: 'Invalid Token!!' });
     }
 
-    return req;
+    next();
   },
-}
+  signToken: function ({ username, email, _id }) {
+    const payload = { username, email, _id };
+
+    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+  },
+};
